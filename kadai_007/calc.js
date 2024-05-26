08974fe50f2a2d72657561359626eef4e271d370
kurokimai <164568171+kurokimai@users.noreply.github.com>
2024-05-26 14:22:21 +0900

    (再)kadai_007アップロード

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -4,17 +4,24 @@ let num = Math.floor(Math.random() * 16);
 //変数numの値を出力する(確認用)
 console.log(num);
 
+if (num % 3 === 0 && num % 5 === 0){
+  console.log('3と5の倍数です');
+}
+
 //変数numの値が３の倍数であれば「3の倍数です」という文字列を出力する
-if (num === 3 || num === 6 || num === 9 || num === 12) {
+else if (num % 3 === 0) {
   console.log('3の倍数です');
 }
 
 //変数unmの値が５の倍数であれば「5の倍数です」という文字列を出力する
-else if (num === 5 || num === 10){
+else if (num % 5 === 0){
   console.log('5の倍数です');
 }
 
-else if (num === 15){
-  console.log('3と5の倍数です');
+
+else {
+  console.log(num);
 }
+
+
 
